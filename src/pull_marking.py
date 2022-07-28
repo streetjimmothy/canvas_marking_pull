b7992b98e447b8f0b578f7b1e18f33052de511bd
Charlotte Pierce <charlotte@malformed-bits.com>
2022-07-28 16:53:39 +1000

    Generating a summary csv.
    Signed-off-by: Charlotte Pierce <charlotte@malformed-bits.com>

--- src/pull_marking.py
+++ src/pull_marking.py
@@ -47,14 +47,43 @@ def _get_all_valid_submissions(course):
             and s.submitted_at_date <= a.due_at_date # it's not overdue
             and (s.graded_at is None or s.graded_at_date < s.submitted_at_date)] # it hasn't ever been graded, or the grade there is for an older submission
         valid_submissions[a] = submissions
-
-    for k, v in valid_submissions.items():
-        print(k, len(v))
     
     return valid_submissions
+
+
+def __speedgrader_link(course, assignment, submission):
+    '''Get a speedgrader formatted link for viewing a specific submission on an assignment.'''
+
+    return "https://swinburne.instructure.com/courses/" + str(course.id) + "/gradebook/speed_grader?assignment_id=" + str(assignment.id) + "&student_id=" + str(submission.user_id)
+
+
+def _generate_csv(course, valid_submissions):
+    '''Generate a csv called "marking.csv containing a summary of tasks that need feedback,
+    and links to the speedgrader to show each task.
+    '''
+
+    student_details = {} # cache student details to avoid unnecessary requests; {userid: ("name, student_id")}
+
+    with open("marking.csv", "w+") as f:
+        f.write("Task, Student, Student ID, Submitted on, Times assessed, Link\n")
+        for assignment, submissions in valid_submissions.items():
+            for s in submissions:
+                student_string = student_details.get(s.user_id)
+                if not student_string:
+                    user = course.get_user(s.user_id)
+                    student_string = user.name + "," + str(user.login_id)
+                    student_details[s.user_id] = student_string
+
+                f.write(assignment.name + ","
+                    + student_string + ","
+                    + s.submitted_at_date.strftime("%d %B") + ","
+                    + str(s.attempt) + ","
+                    + __speedgrader_link(course, assignment, s)
+                    + "\n")
 
 
 if __name__ == "__main__":
     course = _load_course()
     print("Retrieving submissions for ", course.name, "needing feedback...")
     valid_submissions = _get_all_valid_submissions(course)
+    _generate_csv(course, valid_submissions)
